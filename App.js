10a4581f991d4505540cf5a012ea78796fd5204c
Fiyin20 <142032833+Fiyingithub@users.noreply.github.com>
2025-06-19 22:14:57 +0100

    Update App.js

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ app.use("/uploads", express.static("uploads"));
 
 // CORS
 const corsOptions = {
-  origin: ["*", "http://localhost:4219"],
+  origin: ["*"],
   credentials: true,
   optionSuccessStatus: 200,
 };
